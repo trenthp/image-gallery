8702be164ee37e6df0201452afd5729e0aa754d4
Trent Petersen <Foxtrot@Foxtrot.local>
2014-03-02 19:16:33 -0800

    finished with main page

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,29 @@
-$('.curtains').curtain();
 
-$(function () {
-    $('.curtains').curtain({
-        scrollSpeed: 400
-    });
-});
+
+	// Grid View
+	$('#grid-view').on('click', function(){
+
+		// Remove default class, 'large-6'
+		$('.gallery div').removeClass('large-6');
+
+		// Add 'large-3' class
+		$('.gallery div').addClass('large-3');
+
+		// Hide Paragraphs
+		$('.gallery p').hide();
+
+	});
+
+	// List View
+	$('#list-view').on('click', function(){
+
+		// Remove existing class of 'large-3'
+		$('.gallery div').removeClass('large-3');
+
+		// Add 'large-6' class
+		$('.gallery div').addClass('large-6');
+
+		// Show paragraphs
+		$('p').show();
+
+	});
